f86b67edacf4d9e4172168fc7a604235f957a212
mohamed elmarbouh <125465547+Aminuv@users.noreply.github.com>
2023-11-05 18:05:45 +0000

    Update 3-hbnb

--- web_dynamic/3-hbnb.py
+++ web_dynamic/3-hbnb.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Starts a Flash Web Application """
+""" The starts a Flash Web Application """
 from models import storage
 from models.state import State
 from models.city import City
@@ -9,19 +9,17 @@ from os import environ
 from flask import Flask, render_template
 import uuid
 app = Flask(__name__)
-# app.jinja_env.trim_blocks = True
-# app.jinja_env.lstrip_blocks = True
 
 
 @app.teardown_appcontext
 def close_db(error):
-    """ Remove the current SQLAlchemy Session """
+    """ To remove the current SQLAlchemy Session """
     storage.close()
 
 
 @app.route('/3-hbnb', strict_slashes=False)
 def hbnb():
-    """ HBNB is alive! """
+    """ ''HBNB' is alive! """
     states = storage.all(State).values()
     states = sorted(states, key=lambda k: k.name)
     st_ct = []
