e84c523c60ff6738e50bdbac09d139ad6b943383
mohamed elmarbouh <125465547+Aminuv@users.noreply.github.com>
2023-11-05 17:49:44 +0000

    Update 0-hbnb

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Starts a Flash Web Application """
+""" The starts a Flash Web Application """
 from models import storage
 from models.state import State
 from models.city import City
@@ -9,9 +9,8 @@ from os import environ
 from flask import Flask, render_template
 import uuid
 app = Flask(__name__)
-# app.jinja_env.trim_blocks = True
-# app.jinja_env.lstrip_blocks = True
 
+# The : app jinja env.trim_blocks = True
 
 @app.teardown_appcontext
 def close_db(error):
