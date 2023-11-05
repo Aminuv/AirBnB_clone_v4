8f438e515eb85b0a365354aad29be42b23deefb6
mohamed elmarbouh <125465547+Aminuv@users.noreply.github.com>
2023-11-05 18:04:50 +0000

    Update 2-hbnb

--- web_dynamic/2-hbnb.py
+++ web_dynamic/2-hbnb.py
@@ -14,13 +14,13 @@ app = Flask(__name__)
 
 @app.teardown_appcontext
 def close_db(error):
-    """ Remove the current SQLAlchemy Session """
+    """ remove the current SQLAlchemy Session """
     storage.close()
 
 
 @app.route('/2-hbnb', strict_slashes=False)
 def hbnb():
-    """ HBNB is alive! """
+    """ 'HBNB' is alive! """
     states = storage.all(State).values()
     states = sorted(states, key=lambda k: k.name)
     st_ct = []
